a2457e8dd77223506cdb267b240104ebdd82b132
knightchaser <agerio100@naver.com>
2025-11-08 23:05:30 +0900

    Dev: Exercise more about struct association functions

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -4,22 +4,45 @@ struct Rectangle {
     height: u32,
 }
 
+// A set of Rectangle-structure-related functions
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    // An association function which is not a method; it does not take &self as a parameter
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let scale: u32 = 2;
     let rect1 = Rectangle {
         width: dbg!(30 * scale),
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle::square(55);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
 
     // println!("Rect1 object  => {:#?}", rect1);
     dbg!(&rect1);
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
